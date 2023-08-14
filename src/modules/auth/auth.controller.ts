7e2d1c17a9fa5e334b94c62084680b42fd25265d
Obaid <obaid3035@gmail.com>
2023-08-15 01:13:42 +0200

    Added Login Api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { createUserDto } from "./dto/createUserDto";
+import { LoginUserDto } from "./dto/loginUserDto";
 
 
 @Controller('auth')
@@ -12,6 +13,13 @@ export class AuthController {
   async register(@Body() body: createUserDto) {
     return {
       token: await this.authService.register(body),
+    }
+  }
+
+  @Post('login')
+  async login(@Body() body: LoginUserDto) {
+    return {
+      token: await this.authService.login(body)
     }
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,19 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { SupabaseService } from "../supabase/supabase.service";
 import { createUserDto } from "./dto/createUserDto";
-import { hash } from 'bcrypt';
+import { hash, compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { ConfigService } from "@nestjs/config";
 import { PostgrestError } from "@supabase/supabase-js";
+import { LoginUserDto } from "./dto/loginUserDto";
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly supabaseService: SupabaseService,
-    private readonly configService: ConfigService,
-  ) {}
+    private readonly configService: ConfigService
+  ) {
+  }
 
   async register(newUser: createUserDto): Promise<string> {
     console.log("********** Registering user ***********");
@@ -24,12 +26,12 @@ export class AuthService {
 
     let hashedPassword: string | undefined;
     if (password) {
-      hashedPassword =  await hash(password, 10)
+      hashedPassword = await AuthService.hashPassword(password);
     }
 
     await this.supabaseService.getSupabase().from("User").insert<createUserDto>({
       ...newUser,
-      password: hashedPassword,
+      password: hashedPassword
     });
 
     const { data } = await this.supabaseService
@@ -39,9 +41,32 @@ export class AuthService {
       .eq("email", email);
 
     console.log("********** User Registered Successfully ***********");
-    return sign({ id: data[0].id }, this.configService.get("JWT_SECRET"));
+    return this.generateToken(data[0].id)
   }
 
+  async login(userData: LoginUserDto): Promise<string> {
+    console.log("********** Logging user ***********");
+    const user = await this.authenticate(userData);
+    console.log("********** User logged in Successfully ***********");
+    return this.generateToken(user.id);
+  }
+  async authenticate(userData: LoginUserDto) {
+    const { data } = await this.supabaseService.getSupabase()
+      .from("User")
+      .select()
+      .eq("email", userData.email);
+
+    if (data.length <= 0) {
+      throw new NotFoundException("Unable too login. Please registered yourself");
+    }
+
+    const isMatch: boolean = await compare(userData.password, data[0].password);
+
+    if (!isMatch) {
+      throw new BadRequestException("Email or Password is incorrect");
+    }
+    return data[0];
+  }
   private async isEmailAlreadyUsed(email: string): Promise<boolean | PostgrestError> {
     const { data, error } = await this.supabaseService
       .getSupabase()
@@ -51,6 +76,12 @@ export class AuthService {
       .limit(1);
 
     return data.length > 0 || error;
+  }
+  private static async hashPassword(password: string): Promise<string> {
+    return hash(password, 10);
+  }
+  private async generateToken(id): Promise<string> {
+    return sign({ id }, this.configService.get("JWT_SECRET"));
   }
 
 }
